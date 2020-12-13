1dcaa08d18905cf214d548a2159c5ba9085cc7ba
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-12-14 00:05:19 +0700

    fix: search ground and user status

--- src/services/ground.service.ts
+++ src/services/ground.service.ts
@@ -143,7 +143,7 @@ class GroundService {
             subGround.orders.forEach((order: any) => {
               console.log('here------------------order.startDay--------', order.startDay, startDay);
               // compare start time with start day and start time of order
-              if (moment(startTime, 'HH:mm:ss').diff(moment(order.startTime, 'HH:mm:ss')) === 0 && moment(order.startDay, 'DD-MM-YYYY').isSame(startDay)) {
+              if (moment(startTime, 'HH:mm:ss').diff(moment(order.startTime, 'HH:mm:ss')) === 0 && moment(order.startDay).isSame(startDay)) {
                 isReady = false;
                 return [...acc]; // REMOVE GROUND ALSO
               }
